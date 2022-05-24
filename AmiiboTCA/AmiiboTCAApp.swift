4469fbce673bdadbd70847bbb0db72d360ded548
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-05-24 08:24:20 -0700

    First commit

---
+++ AmiiboTCA/AmiiboTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmiiboTCAApp.swift
+//  AmiiboTCA
+//
+//  Created by Pedro Rojas on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AmiiboTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmiiboTCA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmiiboTCA
+//
+//  Created by Pedro Rojas on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
