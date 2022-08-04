2973aaa3efe80b8a7460660ea18cd8c84a2a66db
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-08-04 09:05:06 -0700

    Rename project to OnlineStoreTCA

---
+++ OnlineStoreTCA/OnlineStoreTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreTCAApp.swift
+//  OnlineStoreTCA
+//
+//  Created by Pedro Rojas on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
