b031e4dfe425bb6ff8955fd8a566b8e66731102a
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-08-27 08:23:06 -0700

    Add test for AddToCartDomain

---
+++ OnlineStoreTCA/Products/AddToCart/AddToCartDomainTest.swift
@@ -0,0 +1,77 @@
+//
+//  AddToCartDomainTest.swift
+//  OnlineStoreTCA
+//
+//  Created by Pedro Rojas on 26/08/22.
+//
+
+import ComposableArchitecture
+import XCTest
+
+@testable import OnlineStoreTCA
+
+@MainActor
+class AddToCartDomainTest: XCTestCase {
+    
+    func testIncreaseCounterTappingPlusButtonOnce() async {
+        let store = TestStore(
+            initialState: AddToCartDomain.State(),
+            reducer: AddToCartDomain.reducer,
+            environment: AddToCartDomain.Environment()
+        )
+        
+        
+        await store.send(.didTapPlusButton) {
+            $0.count = 1
+        }
+    }
+    
+    func testIncreaseCounterTappingPlusButtonThreeTimes() async {
+        let store = TestStore(
+            initialState: AddToCartDomain.State(),
+            reducer: AddToCartDomain.reducer,
+            environment: AddToCartDomain.Environment()
+        )
+        
+        
+        await store.send(.didTapPlusButton) { $0.count = 1 }
+        await store.send(.didTapPlusButton) { $0.count = 2 }
+        await store.send(.didTapPlusButton) { $0.count = 3 }
+    }
+    
+    func testDecreaseCounterTappingPlusButtonOnce() async {
+        let store = TestStore(
+            initialState: AddToCartDomain.State(),
+            reducer: AddToCartDomain.reducer,
+            environment: AddToCartDomain.Environment()
+        )
+        
+        await store.send(.didTapMinusButton) {
+            $0.count = -1
+        }
+    }
+    
+    func testDecreaseCounterTappingPlusButtonThreeTimes() async {
+        let store = TestStore(
+            initialState: AddToCartDomain.State(),
+            reducer: AddToCartDomain.reducer,
+            environment: AddToCartDomain.Environment()
+        )
+        
+        await store.send(.didTapMinusButton) { $0.count = -1 }
+        await store.send(.didTapMinusButton) { $0.count = -2 }
+        await store.send(.didTapMinusButton) { $0.count = -3 }
+    }
+    
+    func testUpdatingCounterTappingPlusAndMinusButtons() async {
+        let store = TestStore(
+            initialState: AddToCartDomain.State(),
+            reducer: AddToCartDomain.reducer,
+            environment: AddToCartDomain.Environment()
+        )
+        
+        await store.send(.didTapMinusButton) { $0.count = -1 }
+        await store.send(.didTapPlusButton) { $0.count = 0 }
+        await store.send(.didTapMinusButton) { $0.count = -1 }
+    }
+}
