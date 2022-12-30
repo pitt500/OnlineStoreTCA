d90e9d52c0ed175f2a4765275f0f218d8143b5c5
Pedro Rojas <pedro.rojaslo50@gmail.com>
2022-12-29 18:52:10 -0800

    Add urls instead of static images

--- OnlineStoreTCA/Products/Product/Product.swift
+++ OnlineStoreTCA/Products/Product/Product.swift
@@ -49,7 +49,7 @@ extension Product {
                 price: 22.3,
                 description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
                 category: "men's clothing",
-                imageString: "tshirt"
+                imageString: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
             ),
             .init(
                 id: 2,
@@ -57,7 +57,7 @@ extension Product {
                 price: 109.95,
                 description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
                 category: "men's clothing",
-                imageString: "bag"
+                imageString: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
             ),
             .init(
                 id: 3,
@@ -65,7 +65,7 @@ extension Product {
                 price: 55.99,
                 description: "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
                 category: "men's clothing",
-                imageString: "jacket"
+                imageString: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
             )
         ]
     }
